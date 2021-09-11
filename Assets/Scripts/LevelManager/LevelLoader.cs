9bd5e0fad2d1536b06b28cc079ed590c77684923
Charbel Al Sahlani <charbel.alsahlani@gmail.com>
2021-09-11 16:05:56 +0300

    Added a simple level loader cross fade

---
+++ Assets/Scripts/LevelManager/LevelLoader.cs
@@ -0,0 +1,53 @@
+/**
+ *
+ * @file		LevelLoader.cs
+ * @brief		
+ * @details
+ * @author		Charbel Al Sahlani (charbel.alsahlani@gmail.com)
+ * @date		Sep 11, 2021
+ * @note        
+ * @see        
+ * @version 	1
+ * @warning     
+ * @copyright
+ * 2021 Team Dark Matter
+ * 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    //Reference to the crossFade animator
+    public Animator transition;
+    //Time to wait while the animation is playing
+    public float transitionTime = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        //This is just for testing and will be deleted later :)
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int leveIndex)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load scene
+        SceneManager.LoadScene(leveIndex);
+    }
+}
