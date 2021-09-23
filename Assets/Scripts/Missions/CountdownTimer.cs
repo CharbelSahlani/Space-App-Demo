7183046dbc89223628c646a399c2003ecd280c1e
Charbel Al Sahlani <charbel.alsahlani@gmail.com>
2021-09-23 12:47:49 +0300

    enhanced the countdown timer

--- Assets/Scripts/Missions/CountdownTimer.cs
+++ Assets/Scripts/Missions/CountdownTimer.cs
@@ -24,12 +24,16 @@ public class CountdownTimer : MonoBehaviour
     public bool countdownIsActive = false;
     public float startingTime = 0f;
     float currentTime = 0f;
+    int minutesRemaining = 0;
+    int secondsRemaining = 0;
     // Current time but in integer
     int currentTimeInt = 0;
     [SerializeField]
     private TextMeshProUGUI countdownText;
     // To play the counting sound
     private bool soundOn = true;
+    // Critical time animator
+    public Animator criticalTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -46,23 +50,7 @@ public class CountdownTimer : MonoBehaviour
         if (countdownIsActive)
         {
             currentTime -= 1 * Time.fixedDeltaTime;
-            currentTimeInt = (int) currentTime;
-            // Start counting sound from 10 seconds
-            if (currentTimeInt < 11 && soundOn)
-            {
-                soundOn = false;
-                countdownText.color = Color.red;
-                StartCoroutine(PlayCountdownSound());
-            }
-            // Print the time on the UI
-            countdownText.text = currentTimeInt.ToString();
-            if (currentTime <= 0)
-            {
-                countdownIsActive = false;
-                //TODO Trigger the GameOver method
-                AudioManager.instance.MusicMixer.SetFloat("MusicVolume", -80f);
-                AudioManager.instance.PlaySound("Mission Failed");
-            }
+            DisplayTimer(currentTime);
         }
     }
 
@@ -75,6 +63,38 @@ public class CountdownTimer : MonoBehaviour
         {
             AudioManager.instance.PlaySound("Countdown");
             yield return new WaitForSeconds(1f);
+        }
+    }
+
+    /**
+     * This function displays the remaining time in this
+     * format 00:00
+     * param [in] currentTime   represents the current time
+     */
+    private void DisplayTimer(float currentTime)
+    {
+        currentTimeInt = (int)currentTime;
+        minutesRemaining = currentTimeInt / 60;
+        secondsRemaining = currentTimeInt % 60;
+        // Play the counting sound starting from 10 seconds
+        if (currentTimeInt < 11 && soundOn)
+        {
+            soundOn = false;
+            countdownText.color = Color.red;
+            //Play animation
+            criticalTime.SetTrigger("criticalTime");
+            StartCoroutine(PlayCountdownSound());
+        }
+        // Print the time on the UI in this format 00:00
+        countdownText.text = string.Format("{0:00}:{1:00}", minutesRemaining, secondsRemaining);
+        if (currentTime <= 0)
+        {
+            countdownIsActive = false;
+            //TODO Trigger the GameOver method
+            // Stop animation
+            criticalTime.SetTrigger("timeEnded");
+            AudioManager.instance.MusicMixer.SetFloat("MusicVolume", -80f);
+            AudioManager.instance.PlaySound("Mission Failed");
         }
     }
 }
