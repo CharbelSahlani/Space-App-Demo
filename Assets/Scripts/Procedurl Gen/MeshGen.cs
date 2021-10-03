ae97b9285e6063cfb340917df2a49dad1628688b
Charbel Al Sahlani <charbel.alsahlani@gmail.com>
2021-10-03 19:29:22 +0300

    modified meshfgen script

--- Assets/Scripts/Procedurl Gen/MeshGen.cs
+++ Assets/Scripts/Procedurl Gen/MeshGen.cs
@@ -35,11 +35,16 @@ public class MeshGen : MonoBehaviour
         GetComponent<MeshFilter>().mesh = mesh;
         generate_mesh();
         update_mesh();
-
+        /*
         colX.transform.position = new Vector3(world_x - collider_offset, colX.transform.position.y, world_z/2);
         col_X.transform.position = new Vector3(collider_offset, col_X.transform.position.y, world_z/2);
         colZ.transform.position = new Vector3(world_x/2, colZ.transform.position.y, world_z - collider_offset);
         col_Z.transform.position = new Vector3(world_z/2, col_Z.transform.position.y, collider_offset);
+        */
+        colX.transform.position = new Vector3(world_x, colX.transform.position.y, world_z / 2);
+        col_X.transform.position = new Vector3(0f, col_X.transform.position.y, world_z / 2);
+        colZ.transform.position = new Vector3(world_x / 2, colZ.transform.position.y, world_z);
+        col_Z.transform.position = new Vector3(world_z / 2, col_Z.transform.position.y, 0f);
         col_Z.GetComponent<BoxCollider>().size = new Vector3(world_z, 100, 0.5f);
         colZ.GetComponent<BoxCollider>().size = new Vector3(world_z, 100, 0.5f);
         colX.GetComponent<BoxCollider>().size = new Vector3(world_x, 100, 0.5f);
@@ -50,7 +55,8 @@ public class MeshGen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<MeshCollider>().convex = true;
+        Debug.Log(GetComponent<MeshCollider>().convex);
     }
 
     void generate_mesh()
@@ -69,7 +75,7 @@ public class MeshGen : MonoBehaviour
         int tris = 0;
         int verts = 0;
 
-        for (int i = 0, z = 0; z < world_z; z++)
+        for (int z = 0; z < world_z; z++)
         {
             for (int x = 0; x < world_x; x++)
             {
